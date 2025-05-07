05b9a3544d0dd799e1cfe76d44b9effbfe4dcab9
maisam-mi <maisamhazara2018@gmail.com>
2025-05-07 19:38:51 +0200

    The basic requests are prepared in server. The index.html is also created.

--- server/server.js
+++ server/server.js
@@ -5,9 +5,74 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'files')));
 
+const url = 'https://api.themoviedb.org/3/authentication';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZmFkNDlhMGQ1ZmFjMjg1ODc5MGUxYWUzMTQzYzY2ZSIsIm5iZiI6MS43NDIyMzYxMDM2NTc5OTk4ZSs5LCJzdWIiOiI2N2Q4NjljNzAyZTVhYWM0NDMwMTE0YjkiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.utCg0oq0jdlR6AjmPNyIqcdclwsIfVwmlMUbPZ867gA',
+  },
+};
+
 app.get('/hello', (req, res) => {
-    res.send('Hello Maisam!');
-})
+  res.send('Hello BugBusters!');
+});
+
+// it responses an array of right now highlighted movies. 
+app.get('/movies/highlight', async (req, res) => {
+  try {
+    const response = await fetch('https://api.themoviedb.org/3/movie/popular', options);
+    const movies = (await response.json()).results;
+    res.send(movies);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error fetching movies');
+  }
+});
+
+// it responses an object for the details of a movie.
+// paramters: 
+//      movieId => the id of the movie.
+app.get('/movies/details/:movieId', async (req, res) => {
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${req.params.movieId}`, options);
+      const movie = await response.json();
+      res.send(movie);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('Error fetching details of a movie');
+    }
+});
+
+// it responses an array of movies as result of searching through genres.
+// query: 
+//      genreId => id of genre. if theres more genres, give the ids seperated with comma. e.g. : 12,28
+app.get('/movies/search', async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie?with_genres=${req.query.genreId}`,
+      options,
+    );
+    const movies = (await response.json()).results;
+    res.send(movies);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error fetching movies of search request');
+  }
+});
+
+// it responses an array of all genres. 
+app.get('/genres', async (req, res) => {
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list`, options);
+    const genres = (await response.json()).genres;
+    res.send(genres);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error fetching genres');
+  }
+});
 
 app.listen(3100);
 
